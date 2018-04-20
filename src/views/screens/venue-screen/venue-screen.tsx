c4682d9d756ad600fbbe90790ba59c20a0468165
Kevin VanGelder <kevin@infinite.red>
2018-04-19 19:43:26 -0700

    basic venue screen

--- src/views/screens/venue-screen/venue-screen.tsx
+++ src/views/screens/venue-screen/venue-screen.tsx
@@ -1,10 +1,28 @@
 import * as React from "react"
+import { Image, View, ImageStyle, TextStyle, ViewStyle, ScrollView } from "react-native"
 import { Text } from "../../shared/text"
 import { NavigationScreenProps } from "react-navigation"
 import { Screen } from "../../shared/screen"
 import { TitleBar } from "../../../views/shared/title-bar"
+import { spacing } from "../../../theme/spacing"
 
 export interface VenueScreenProps extends NavigationScreenProps<{}> {}
+
+const ROOT: ViewStyle = {
+  flex: 1,
+  padding: spacing[2],
+}
+const TEXT: TextStyle = {
+  marginBottom: spacing[3],
+  textAlign: "center",
+}
+const IMAGE: ImageStyle = {
+  flex: 1,
+  resizeMode: "contain",
+  maxWidth: "100%",
+  maxHeight: "200%",
+  marginBottom: spacing[5],
+}
 
 export class VenueScreen extends React.Component<VenueScreenProps, {}> {
   static navigationOptions = {
@@ -13,8 +31,15 @@ export class VenueScreen extends React.Component<VenueScreenProps, {}> {
 
   render() {
     return (
-      <Screen preset="fixedCenter">
-        <Text preset="header" tx="venueScreen.header" />
+      <Screen preset="fixed" style={ROOT}>
+        <ScrollView>
+          <Text preset="header" text="First Floor" style={TEXT} />
+          <Image source={require("./floor-1.png")} style={IMAGE} />
+          <Text preset="header" text="Second Floor" style={TEXT} />
+          <Image source={require("./floor-2.png")} style={IMAGE} />
+          <Text preset="header" text="Third Floor" style={TEXT} />
+          <Image source={require("./floor-3.png")} style={IMAGE} />
+        </ScrollView>
       </Screen>
     )
   }
